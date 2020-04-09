78c628a36e37858b30ee76277bb05565b854273c
David ouanounou <david.ouanounou@easy.movie>
2020-04-09 12:09:41 +0200

    Layouts

---
+++ src/components/layouts/box.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+const Box = styled.div`
+    width: 200px;
+    height: 100px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border: 1px solid #e5e7eb;
+    border-radius: 4px;
+    background: #fff;
+    padding: 16px;
+`;
+
+export default Box;
